9ce37034c32e5fbb88265d195df9e6b2438f26d4
aniruddhasawant <aniruddhasawant@localhost>
2020-05-06 23:21:41 +1000

    2nd commit

--- counter-api/src/main/java/com/sawant/counterapi/controller/CounterController.java
+++ counter-api/src/main/java/com/sawant/counterapi/controller/CounterController.java
@@ -1,10 +1,31 @@
 package com.sawant.counterapi.controller;
 
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sawant.counterapi.service.CounterService;
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/counter-api")
 public class CounterController {
 
+	@Autowired
+	CounterService cs;
+	
+	@GetMapping("/")
+	public String index() {
+		return "welcome";
+	}
+	
+	@PostMapping(path = "/search")
+	public Map<String,Long> getSearchCount(@RequestBody List<String> searchText) {
+		return cs.getSearchCount(searchText);
+	}
+	
 }
--- counter-api/src/main/java/com/sawant/counterapi/service/CounterService.java
+++ counter-api/src/main/java/com/sawant/counterapi/service/CounterService.java
@@ -1,5 +1,35 @@
 package com.sawant.counterapi.service;
 
-public class CounterService {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class CounterService {
+	private static final Logger LOGGER = Logger.getLogger( CounterService.class.getName() );
+	static List<String> wordList;
+	static {
+		try (Stream<String> lines = Files.lines(Paths.get("/resources/Paragraph.txt"))) {
+			wordList = lines.collect(Collectors.toList());
+		} catch (IOException e) {
+			LOGGER.fine("Unable to acces the text file.");
+		}
+	}
+	
+	public Map<String,Long> getSearchCount(List<String> searchWords) {
+		Map<String,Long> resultMap=new HashMap<>();
+		for(String str:searchWords) {
+			resultMap.put(str, wordList.stream().filter(s->s.equalsIgnoreCase(str)).count());
+		}
+		return resultMap;
+	}
+	
 }
