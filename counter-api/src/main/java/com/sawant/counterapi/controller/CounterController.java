0d9e9aeb4241671791e3066308c05a32e866dbff
aniruddhasawant <aniruddhasawant@aniruddhas-mbp.hub>
2020-05-08 20:39:43 +1000

    Authentication Classes added

--- counter-api/src/main/java/com/sawant/counterapi/controller/CounterController.java
+++ counter-api/src/main/java/com/sawant/counterapi/controller/CounterController.java
@@ -3,7 +3,6 @@ package com.sawant.counterapi.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- counter-api/src/main/java/com/sawant/counterapi/model/WordCount.java
+++ counter-api/src/main/java/com/sawant/counterapi/model/WordCount.java
@@ -1,31 +1,28 @@
 package com.sawant.counterapi.model;
 
-public class WordCount implements Comparable<WordCount> {
-	
+public class WordCount {
+
 	private String word;
 	private Long count;
-	
+
 	public WordCount(String str, long count2) {
-		this.word=str;
-		this.count=count2;
+		this.word = str;
+		this.count = count2;
 	}
-	
+
 	public String getWord() {
 		return word;
 	}
+
 	public void setWord(String word) {
 		this.word = word;
 	}
+
 	public Long getCount() {
 		return count;
 	}
+
 	public void setCount(Long count) {
 		this.count = count;
 	}
-
-	@Override
-	public int compareTo(WordCount newCount) {
-		return count.compareTo(newCount.getCount());
-	}
-
 }
